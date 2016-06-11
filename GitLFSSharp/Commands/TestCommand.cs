d3dd0f37682e9badbce6d6a311b4a29f9611ff14
nori Seokju Yun <noricube@gmail.com>
2016-06-11 22:39:06 +0900

    TestCommand에서 불필요한 내용 제거

--- GitLFSSharp/Commands/TestCommand.cs
+++ GitLFSSharp/Commands/TestCommand.cs
@@ -10,22 +10,6 @@ namespace GitLFSSharp.Commands
     {
         public Task<int> Run(string[] args)
         {
-            List<SolGitLFS.Entities.LFSPointer> lfsObjects = new List<SolGitLFS.Entities.LFSPointer>();
-            lfsObjects.Add(new SolGitLFS.Entities.LFSPointer()
-            {
-                oid = "c9032aeee0352e71587eb73095c92454c7a8710372481e07abf49f17579a3482",
-                size = 16384
-            });
-
-            var task = SolGitLFS.Apis.HTTPApi.BatchDownloadQueryAsync("https://github.com/teamnop/LFSTestRepo.git",
-                lfsObjects);
-            task.Wait();
-
-            var downloadTask = SolGitLFS.Apis.HTTPApi.DownloadAsync(task.Result.objects[0].actions.download.href);
-            downloadTask.Wait();
-
-            Console.WriteLine(downloadTask.Result.Length);
-
             return Task.FromResult<int>(0);
         }
     }
