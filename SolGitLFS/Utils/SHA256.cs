ba1672691deee55cefdadea0671dfdb9abb91bfe
nori Seokju Yun <noricube@gmail.com>
2016-06-13 01:50:04 +0900

    SHA256 Hash를 Stream에서 구하는 함수 추가

--- SolGitLFS/Utils/SHA256.cs
+++ SolGitLFS/Utils/SHA256.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,21 @@ namespace SolGitLFS.Utils
             using (var sha256 = System.Security.Cryptography.SHA256Managed.Create())
             {
                 var hash = sha256.ComputeHash(data);
+
+                return BitConverter.ToString(hash).Replace("-", String.Empty).ToLower();
+            }
+        }
+
+        /// <summary>
+        /// sha256 hash를 계산
+        /// </summary>
+        /// <param name="stream">hash를 구할 stream</param>
+        /// <returns>string으로 변환한 sha256 hash</returns>
+        public static string CalcHash(Stream stream)
+        {
+            using (var sha256 = System.Security.Cryptography.SHA256Managed.Create())
+            {
+                var hash = sha256.ComputeHash(stream);
 
                 return BitConverter.ToString(hash).Replace("-", String.Empty).ToLower();
             }
