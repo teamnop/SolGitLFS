b54fd423053abf86bb800a87fcf9dd429493175e
nori Seokju Yun <noricube@gmail.com>
2016-06-12 13:52:23 +0900

    SHA256 관련 유틸 클래스 추가 - 해시 계산 함수

---
+++ SolGitLFS/Utils/SHA256.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolGitLFS.Utils
+{
+    public class SHA256
+    {
+        /// <summary>
+        /// sha256 hash를 계산
+        /// </summary>
+        /// <param name="data">hash를 구할 데이터</param>
+        /// <returns>string으로 변환한 sha256 hash</returns>
+        public static string CalcHash(byte[] data)
+        {
+            using (var sha256 = System.Security.Cryptography.SHA256Managed.Create())
+            {
+                var hash = sha256.ComputeHash(data);
+
+                return BitConverter.ToString(hash).Replace("-", String.Empty).ToLower();
+            }
+        }
+    }
+}
