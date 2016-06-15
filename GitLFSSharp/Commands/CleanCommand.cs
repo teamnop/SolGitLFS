3b78d3a5c9a2566cf52cfd51bb256fd2f641c991
nori Seokju Yun <noricube@gmail.com>
2016-06-16 00:03:02 +0900

    LFS Clean시에 LFS 파일을 .git/lfs 폴더에 복사하여 push 할때 올릴 수 있도록 수정

--- GitLFSSharp/Commands/CleanCommand.cs
+++ GitLFSSharp/Commands/CleanCommand.cs
@@ -18,31 +18,64 @@ namespace GitLFSSharp.Commands
             byte[] buffer = new byte[65536];
             byte[] hashArray = null;
 
+            var root = SolGitLFS.Utils.GitRepo.FindGitRoot();
+            if (root == null)
+            {
+                throw new Exception("Git Repo를 찾지 못하였습니다");
+            }
+
+            var tmpPath = "lfs\\tmp\\objects";
+            Directory.CreateDirectory(Path.Combine(root, tmpPath));
+
+            var tmpName = DateTime.Now.Ticks;
+            var tmpFile = Path.Combine(root, tmpPath, tmpName.ToString());
+
             using (var stdin = Console.OpenStandardInput())
             {
                 int size = 0;
                 using (var sha256 = SHA256Managed.Create())
                 {
-                    while (true)
+                    using (var fp = File.OpenWrite(tmpFile))
                     {
-                        int readBytes = stdin.Read(buffer, 0, buffer.Length);
+                        while (true)
+                        {
+                            int readBytes = stdin.Read(buffer, 0, buffer.Length);
 
-                        size += readBytes;
-                        if (readBytes > 0)
-                        {
-                            sha256.TransformBlock(buffer, 0, readBytes, null, 0);
+                            size += readBytes;
+                            if (readBytes > 0)
+                            {
+                                sha256.TransformBlock(buffer, 0, readBytes, null, 0);
+                                fp.Write(buffer, 0, readBytes);
+                            }
+                            else
+                            {
+                                sha256.TransformFinalBlock(buffer, 0, readBytes);
+                                hashArray = sha256.Hash;
+                                break;
+                            }
                         }
-                        else
-                        {
-                            sha256.TransformFinalBlock(buffer, 0, readBytes);
-                            hashArray = sha256.Hash;
-                            break;
-                        }
+
+                        fp.Close();
                     }
 
-                    var hash = SolGitLFS.Utils.SHA256.ByteArrayToHash(hashArray);
-                    resultDict.Add(new KeyValuePair<string, string>("oid", "sha256:" + hash));
+                    var oid = SolGitLFS.Utils.SHA256.ByteArrayToHash(hashArray);
+
+                    var relativeLfsObjectPath = SolGitLFS.Utils.GitRepo.CreateLFSObjectPath(oid);
+                    Directory.CreateDirectory(Path.Combine(root, relativeLfsObjectPath));
+
+                    var lfsObjectPath = Path.Combine(root, relativeLfsObjectPath, oid);
+                    if (File.Exists(lfsObjectPath) == false)
+                    {
+                        File.Move(tmpFile, Path.Combine(root, relativeLfsObjectPath, oid));
+                    }
+                    else
+                    {
+                        File.Delete(tmpFile);
+                    }
+                    
+                    resultDict.Add(new KeyValuePair<string, string>("oid", "sha256:" + oid));
                     resultDict.Add(new KeyValuePair<string, string>("size", size.ToString()));
+
                 }
             }
 
